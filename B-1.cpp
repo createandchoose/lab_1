6651fe929180f6f10e3b12cf6a001e47b45dd733
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2023-09-18 13:25:35 +0900

    Update B-1.cpp

--- B-1.cpp
+++ B-1.cpp
@@ -2,19 +2,16 @@
 #include <cmath>
 
 int main() {
-    double circumference; // Длина окружности
+    double circumference;
     std::cout << "Введите длину окружности: ";
     std::cin >> circumference;
 
     const double pi = 3.14159265359; // Пи (можно воспользоваться библиотечной константой M_PI)
 
-    // Вычисляем радиус круга
     double radius = circumference / (2 * pi);
 
-    // Вычисляем площадь круга
     double area = pi * std::pow(radius, 2);
 
-    // Выводим результат
     std::cout << "Площадь круга: " << area << std::endl;
 
     return 0;
