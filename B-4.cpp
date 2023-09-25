bd521520752a2e3ac356a0df2ecb3283d92383c5
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2023-09-25 12:54:56 +0900

    Update B-4.cpp

--- B-4.cpp
+++ B-4.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
-#include <cmath>
-
-int findSixthDigit(int number) {
-    int sixthDigit = 0;
-    int numDigits = (int)log10(number) + 1;
-    
-    if (numDigits >= 6) {
-        int position = numDigits - 6;
-        
-        int divisor = pow(10, position); 
-        sixthDigit = (number / divisor) % 10; 
-    }
-    
-    return sixthDigit;
-}
 
 int main() {
-    int number;
+    int n;
+
     std::cout << "Введите семи- или восьмизначное число: ";
-    std::cin >> number;
-    
-    int sixthDigit = findSixthDigit(number);
+    std::cin >> n;
+
+    int sixthDigit = 0;
+
+    if (n >= 1000000 && n <= 99999999) {
+        
+        int lastSixDigits = (n % 1000000) / 1000;
+   
+        sixthDigit = lastSixDigits / 10;
+    }
+    else {
+        std::cout << "Некорректное число! Введите семи- или восьмизначное число." << std::endl;
+        return 0;
+    }
+
     std::cout << "Шестая цифра с конца: " << sixthDigit << std::endl;
-    
+
     return 0;
 }
